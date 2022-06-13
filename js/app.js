10f66811581a40a95dda057ff935448b81aed6de
lecanardjaune <ugiraud@edenschool.fr>
2022-06-13 11:03:55 +0200

    finish technique

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById("menu");
+const ul = document.getElementById("ul")
+
+
+menu.addEventListener("click",()=>{
+    ul.classList.toggle("none")
+})
+
+console.log(menu)
